e67107e2cc7d10e73a92eda3128fbec31133d7bb
OliveiraGusta <gustavo.oliver456@gmail.com>
2024-06-24 08:33:52 -0300

    Mudando de branch

--- src/bankAccount/Menu.java
+++ src/bankAccount/Menu.java
@@ -1,31 +1,32 @@
 package bankAccount;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import bankAccount.model.Account;
+import bankAccount.model.AccountCurrent;
+import bankAccount.model.AccountSavings;
 import bankAccount.util.Colors;
-
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
-		Account user1 = new Account(1, 2, 1, "Kelly", 10000.0f);
+
+		AccountSavings user1 = new AccountSavings(1, 2, 1, "Kelly", 10000.0f, 24);
 		user1.viewAccountDetails();
 		user1.withdraw(200.0f);
 		user1.viewAccountDetails();
 		user1.deposit(2000.0f);
 		user1.viewAccountDetails();
-		
-		Account user2 = new Account(2, 1, 2, "Luis", 20000.0f);
+
+		AccountCurrent user2 = new AccountCurrent(2, 1, 2, "Luis", 20000.0f, 300.0f);
 		user2.viewAccountDetails();
 		user2.withdraw(100.0f);
 		user2.viewAccountDetails();
 		user2.deposit(1000.0f);
 		user2.viewAccountDetails();
-		
+
 		Scanner scanner = new Scanner(System.in);
 		int option;
-		
 
 		while (true) {
 			System.out.printf("""
@@ -33,7 +34,7 @@ public class Menu {
 
 						Javabank - Melhor banco para se estressar
 
-					-----------------------------------------------------%s 
+					-----------------------------------------------------%s
 
 						1 - Criar Conta
 						2 - Listar todas as Contas
@@ -43,13 +44,19 @@ public class Menu {
 						6 - Sacar
 						7 - Depositar
 						8 - Transferir valores entre Contas
-						
+
 						9 - Fechar aplicação
 
 					%s-----------------------------------------------------%s
-					Entre com a opção desejada:""", Colors.TEXT_GREEN_BOLD, Colors.TEXT_WHITE_BOLD, Colors.TEXT_GREEN_BOLD, Colors.TEXT_RESET);
-			
-			option = scanner.nextInt();
+					Entre com a opção desejada:""", Colors.TEXT_GREEN_BOLD, Colors.TEXT_WHITE_BOLD,
+					Colors.TEXT_GREEN_BOLD, Colors.TEXT_RESET);
+			try {
+				option = scanner.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				scanner.nextLine();
+				option = 0;
+			}
 
 			if (option == 9) {
 				System.out.println("\n\nJavabank - Melhor banco para se estressar");
@@ -70,30 +77,25 @@ public class Menu {
 				System.out.println(Colors.TEXT_WHITE_BOLD + "\n\nListar");
 				System.out.println("---------------------------------------------");
 
-
 				break;
 			case 3:
 				System.out.println(Colors.TEXT_WHITE_BOLD + "\n\nConsulta por numero");
 				System.out.println("---------------------------------------------");
-
 
 				break;
 			case 4:
 				System.out.println(Colors.TEXT_WHITE_BOLD + "\n\nAtualizar Conta");
 				System.out.println("---------------------------------------------");
 
-
 				break;
 			case 5:
 				System.out.println(Colors.TEXT_WHITE_BOLD + "\n\nApagar Conta");
 				System.out.println("---------------------------------------------");
 
-
 				break;
 			case 6:
 				System.out.println(Colors.TEXT_WHITE_BOLD + "\n\nSaque");
 				System.out.println("---------------------------------------------");
-
 
 				break;
 			case 7:
@@ -119,9 +121,21 @@ public class Menu {
 	public static void about() {
 		System.out.println("\n-----------------------------------------------------");
 		System.out.println("Projeto Desenvolvido por: ");
-		System.out.println(Colors.TEXT_WHITE_BOLD +  "Gustavo Oliveira - gustavo.oliver456@gmail.com");
+		System.out.println(Colors.TEXT_WHITE_BOLD + "Gustavo Oliveira - gustavo.oliver456@gmail.com");
 		System.out.println(Colors.TEXT_GREEN_BOLD + "github.com/OliveiraGusta");
 		System.out.println("-----------------------------------------------------");
 	}
 
+	public static void keyPress() {
+		try {
+
+			System.out.println(Colors.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
 }
